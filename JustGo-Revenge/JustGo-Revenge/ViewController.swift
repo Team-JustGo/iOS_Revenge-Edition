da0327ef88a6f94e2c2b6d86123af1ec5f013cdf
Woopa01 <whdnwls49@naver.com>
2019-06-24 20:07:57 +0900

    initial commit

---
+++ JustGo-Revenge/JustGo-Revenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustGo-Revenge
+//
+//  Created by 조우진 on 24/06/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
